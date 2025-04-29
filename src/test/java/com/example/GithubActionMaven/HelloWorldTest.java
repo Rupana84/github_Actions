1dada8cb9f463d06206fa2594bfe75fed208b7c6
Rupana <sistajag@gmail.com>
2025-04-30 01:15:08 +0200

    Add HelloWorld test

---
+++ src/test/java/com/example/GithubActionMaven/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.example.GithubActionMaven;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HelloWorldTest {
+
+    @Test
+    void helloWorldTest() {
+        assertTrue(true);
+    }
+}
